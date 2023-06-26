35343e69d275d03d1ec272d0998d9306eddec2b5
hazzlenut <123593498+hazzlenut@users.noreply.github.com>
2023-06-26 20:41:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
+const basePath = '/landing-page';
+
 const nextConfiguration = {
-  basePath: 'landing-page'
+  basePath,
+  assetPrefix: `${basePath}/`,
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
 
