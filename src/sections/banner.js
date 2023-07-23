67a3efa170928e1838aedd5edb5e80adc97e1471
hazzlenut <123593498+hazzlenut@users.noreply.github.com>
2023-07-24 01:59:07 +0530

    Class added

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -13,7 +13,7 @@ import { Link } from 'components/link';
 
 const Banner = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const form = <iframe className="iframe" src="https://docs.google.com/forms/d/e/1FAIpQLScANHuJZG_EqRjs3J02oP0XUndKBiosbzdzip2PHBs28jsFVw/viewform?embedded=true" width="640" height="1470" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  const form = <iframe className={styles.iframe} src="https://docs.google.com/forms/d/e/1FAIpQLScANHuJZG_EqRjs3J02oP0XUndKBiosbzdzip2PHBs28jsFVw/viewform?embedded=true" width="640" height="1470" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
   return (
     <>{isOpen && <Modal children={form} header='Contact Information' setIsOpen={setIsOpen} />}
       <Box sx={styles.banner} id="banner">
